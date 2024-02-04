7dc20a75e29c351a099bd3c7aaf34a6ec0f67d55
Tyler Coulson <tecoulson92@gmail.com>
2024-02-03 19:27:47 -0500

    add delete all letters and add letter

--- app/static/scripts2.js
+++ app/static/scripts2.js
@@ -1,5 +1,6 @@
 import { WORDS } from "./words.js";
 import { buildBoard, populateBoard } from "./build-board.js";
+import { saveJson } from "./save-load.js";
 
 export const MAX_LENGTH = 10;
 export const BOARD_SIZE = 9;
@@ -11,7 +12,7 @@ let strDate = `${currentDate.getFullYear()}-${('0'+currMonth).slice(-2)}-${('0'+
 let words = WORDS[strDate];
 
 let data = {"won":false, "minLengths":[1, 1, 1, 1, 1, 1, 1, 1, 1], "currRow":1};
-let currentLength = data['minLengths'][data['currRow']]
+let currentLength = data['minLengths'][data['currRow']];
 
 function initGame(words, data) {
 
@@ -29,29 +30,66 @@ function insertLetter(letter, cell) {
     if (currentLength >= MAX_LENGTH) {
         return;
     }
+
     cell.textContent = letter.toUpperCase();
     currentLength += 1;
 };
 
-function deleteLetter(cell) {
-    if (data["minLengths"][data['currRow']] > currentLength) {
+function deleteLetter(cell, data) {
+    if (data["minLengths"][data['currRow']] >= currentLength) {
         return;
     }
     cell.textContent = ""
     currentLength -= 1;
 };
 
+function deleteAllLetters(data){
+    for (let i = currentLength; i >= data["minLengths"][data["currRow"]]; i--) {
+        let cell = getCell(i-1);
+        deleteLetter(cell, data);
+    }
+
+};
+
+function addLetter(date, data) {
+    console.log('add', currentLength)
+
+    deleteAllLetters(data);
+    let word = words[data["currRow"]];
+    if (currentLength >= word.length) {
+        return;
+    }
+    let letterID = data["minLengths"][data['currRow']]
+    let key = word[letterID] 
+
+    document.dispatchEvent(new KeyboardEvent("keyup", { key: key }));
+    data["minLengths"][data['currRow']] += 1;
+    getCell(letterID).classList.add("text-warning")
+    saveJson(date, data);
+};
+
+function getRow() {
+    return document.querySelector('input[name="row"]:checked').parentElement
+};
+
+function getCell(id){
+    let row = getRow();
+    let cell = row.getElementsByTagName('div')[id];
+    return cell
+};
+
 document.addEventListener("keyup", (e) => {
     let pressedKey = e.key;
-    let cell = document.querySelector('input[name="row"]:checked').parentElement.getElementsByTagName('div')[currentLength];
-    
 
     if (pressedKey === "Backspace" && currentLength > 0) {
-        deleteLetter(cell);
+        let cell = getCell(currentLength-1)
+        deleteLetter(cell, data);
         return;
     }
 
     if (/^[a-z]$/i.test(pressedKey)) {
+
+        let cell = getCell(currentLength)
         insertLetter(pressedKey, cell);
         return;
     }
@@ -61,6 +99,9 @@ document.addEventListener("keyup", (e) => {
 });
 
 
-document.getElementById('addLetter').addEventListener("click", addLetter);
+document.getElementById('addLetter').addEventListener("click", (e) => {
+    
+    addLetter(strDate, data);
+});
 
 initGame(words, data);
